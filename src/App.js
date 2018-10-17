fd3410ba25bbe27e0cf2035464a38e10634d577c
ougi <bdbkx286@gmail.com>
2018-10-17 12:04:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrReader from 'react-qr-reader';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      delay: 300,
+      result: 'No result'
+    };
+    this.handleScan = this.handleScan.bind(this);
+  }
+
+  handleScan(data) {
+    if (data) {
+      this.setState({
+        result: data
+      });
+    }
+  }
+
+  handleError(err) {
+    console.error(err);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div style={{ width: '500px' }}>
+          <QrReader
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: '100%' }}
+            facingMode="user"
+          />
+          <p>{this.state.result}</p>
+        </div>
       </div>
     );
   }
